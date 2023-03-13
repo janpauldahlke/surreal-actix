3529e3e20cb4cc317114c5f0f6b956b0f8364480
jdahlke <jdahlke@aidminutes.com>
2023-03-13 13:10:57 +0100

    is finally able to verify query params in a test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::http::header;
 use actix_web::HttpRequest;
-use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Responder};
+//use actix_web::{get, test::call_service, test::TestRequest, web, App, HttpResponse, HttpServer};
+use actix_web::{get, test::call_service, test::TestRequest, web, App, HttpResponse, HttpServer};
+//use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Responder};
 
 mod api;
 mod error;
@@ -87,13 +89,14 @@ const _HEADER: &str = "X-SECRET";
 // -- end region
 
 // a mvp of a web server
-#[derive(Debug, Deserialize)]
+#[derive(Debug, serde::Deserialize)]
 pub struct FooParams {
     name: String,
     location: String,
     age: u16,
 }
 
+#[get("/foo")]
 async fn foo_querry_params(query: web::Query<FooParams>) -> HttpResponse {
     let result = format!(
         "route params are, name{} {} {}",
@@ -120,53 +123,17 @@ async fn greet_handler(req: HttpRequest, path: web::Path<String>) -> HttpRespons
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || {
-        App::new()
-            .route("/", web::get().to(index))
-            //.route("/hello/{name}", web::get().to(greet))
-            .route("/hello/{name}", web::get().to(greet_handler))
-            .route("/foo", web::get().to(foo_querry_params))
-    })
-    .bind("127.0.0.1:8081")?
-    .run()
-    .await
+    let server =
+        HttpServer::new(|| App::new().service(foo_querry_params)).bind("127.0.0.1:8080")?;
+
+    server.run().await
 }
 
+#[cfg(test)]
 mod tests {
 
     use super::*;
-    use actix_web::{
-        http::{self, header::ContentType},
-        test::{self, call_service, TestRequest},
-        App,
-    };
-
-    // #[actix_web::test]
-    // async fn test_greet() {
-    //     let app = test::init_service(
-    //         App::new()
-    //             .service(web::resource("127.0.0.1:8081/hello").route(web::get().to(greet_handler))),
-    //     )
-    //     .await;
-
-    //     let req = test::TestRequest::get().uri("/hello/actix").to_request();
-
-    //     // let req = test::TestRequest::post()
-    //     //     .uri("/")
-    //     //     .set_json(&MyObj {
-    //     //         name: "my-name".to_owned(),
-    //     //         number: 43,
-    //     //     })
-    //     //     .to_request();
-    //     // let resp = app.call(req).await.unwrap();
-
-    //     // let result = test::call_and_read_body(&app, req).await;
-    //     // assert_eq!(result, ("Hello actix"));
-
-    //     let result = test::call_and_read_body(&app, req).await;
-
-    //     println!("{:?}", result);
-    // }
+    use actix_web::{test, web, App};
 
     #[actix_web::test]
     async fn test_foo_querry_params() {
@@ -174,9 +141,10 @@ mod tests {
         let request = TestRequest::with_uri("/foo?name=actix&location=world&age=43").to_request();
 
         let response = call_service(&app, request).await;
-        assert_eq!(response.status(), http::StatusCode::OK);
+        assert!(response.status().is_success());
 
         let body = test::read_body(response).await;
+        println!("{:?}", body.clone()); //borrowed
         assert_eq!(body, "route params are, nameactix world 43");
     }
 }
